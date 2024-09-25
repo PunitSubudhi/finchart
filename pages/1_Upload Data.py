8a0a9971064e97f468fb72bdeb9d962c6e0717f5
Punit Subudhi <punitsubudhi@gmail.com>
2024-09-25 22:50:14 +0100

    Bug Fixe : Works when Amount column in uploaded file is not named Amount

--- pages/1_Upload Data.py
+++ pages/1_Upload Data.py
@@ -84,11 +84,14 @@ if file is not None:
     cols=st.columns(num_cols)
     
     if file_type == '+/-':
-        amount_col = st.selectbox("Select the Column that represents Amount",data_columns)
-        
-        data['cr/dr'] = data[amount_col].apply(lambda x: 'Credit' if x > 0 else 'Debit')
-        data[amount_col] = data[amount_col].apply(lambda x: x if x > 0 else -x)
-        st.toast("adding Column cr/dr as Credit/Debit")
+        try:
+            amount_col = st.selectbox("Select the Column that represents Amount",data_columns)
+            
+            data['cr/dr'] = data[amount_col].apply(lambda x: 'Credit' if x > 0 else 'Debit')
+            data[amount_col] = data[amount_col].apply(lambda x: x if x > 0 else -x)
+            st.toast("adding Column cr/dr as Credit/Debit")
+        except Exception as e:
+            st.error(f"Error in Adding Column cr/dr: {e}")
 
     data_columns: list = list(data.columns)
     data_types: list = list(data.dtypes.to_list())
