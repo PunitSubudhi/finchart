ab8875983ba85142029d7e93b1917a30a728b7e7
Punit Subudhi <punitsubudhi@gmail.com>
2024-09-26 21:49:52 +0100

    Bug Fixes

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import streamlit_authenticator as stauth
+import streamlit_option_menu as option_menu
+
+
 
 st.title('Welcome to FinChart | A financial charting app')
 st.sidebar.title('TO Do')
+
+# Add Authentication
+# Add Database Connection
+# Add Data Upload
+# Add Data Processing
+# Add Data Analysis
+# Add Data Visualization
--- pages/1_Upload Data.py
+++ pages/1_Upload Data.py
@@ -118,5 +118,7 @@ if file is not None:
         st.dataframe(parsed_df.head(num_rows))
     # For any data that is not parsed, ask user to correct it with a option to download the wrong data
     # Add Button to continue to next page : Charts
-    st.button("Continue to Charts",key='Continue to Charts',help="Click to continue to the next page to view the charts",on_click=None)
+    #st.button("Continue to Charts",key='Continue to Charts',help="Click to continue to the next page to view the charts",on_click=None)
     
+    # Create charts for the parsed data
+    st.title("Charts")
