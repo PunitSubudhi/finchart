a69c418126da8cc08ba0e0c9a0dec10a68e4fff0
Punit Subudhi <punitsubudhi@gmail.com>
2024-09-26 21:50:35 +0100

    Bug Fixes

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ import streamlit_option_menu as option_menu
 st.title('Welcome to FinChart | A financial charting app')
 st.sidebar.title('TO Do')
 
+st.title('Upload your Transactions below to continue...')
+
 # Add Authentication
 # Add Database Connection
 # Add Data Upload
